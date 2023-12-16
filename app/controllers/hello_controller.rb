02d07ca4c735bc7f9908481ab583d46e5c95e15c
Joseph Lwanzo Khausi <josephlkausi@gmail.com>
2023-12-16 02:40:09 +0200

    Build api of random greetings

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,4 +1,6 @@
 class HelloController < ApplicationController
   def index
+    random_greeting = Message.order('RANDOM()').first
+    render json: { message: random_greeting&.greetings || 'No greetings found' }
   end
 end
